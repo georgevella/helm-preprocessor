0207675464b5d2f174e69e0251f5977c2dd650e3
George Vella <george.vella@riverigaming.com>
2020-02-18 10:13:58 +0100

    Renamed files

---
+++ ArgoCdEnvironmentManager/Configuration/DeploymentConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+
+namespace HelmPreprocessor.Configuration
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class DeploymentConfiguration
+    {
+        public SecretsConfiguration Secrets { get; set; } = new SecretsConfiguration();
+        public ServicesConfiguration Services { get; set; } = new ServicesConfiguration();
+    } 
+
+    public class SecretsConfiguration
+    {
+        public SecretsHandlerType Handler { get; set; } = SecretsHandlerType.Sops;
+
+        public string Filename { get; set; } = "secrets.yaml";
+    }
+
+    public enum SecretsHandlerType
+    {
+        Sops
+    }
+
+    public class ServicesConfiguration : Dictionary<string, ServiceConfiguration>
+    {
+        
+    }
+
+    public class ServiceConfiguration
+    {
+        public string Runtime { get; set; }
+    }
+}
